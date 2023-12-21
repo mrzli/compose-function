460aa97a59b3f15a4b3e8cbb70ded9d73f6ebafe
Goran Mržljak <goran.mrzljak@gmail.com>
2023-12-21 14:46:09 +0100

    change development tooling configs
    Switch to pnpm.
    
    Update various tooling configurations.
    
    Update deps.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,16 @@ All notable changes to the "compose-function" will be documented in this file.
 
 ## [Unreleased]
 
+## [0.0.2] - 2023-12-21
+
+### Changed
+
+- Update dependencies to latest versions.
+- Update tooling configurations.
+
 ## [0.0.1] - 2023-07-15
+
+### Added
 
 - Add `compose` and `composeMono` functions.
 - Initial release.
--- src/compose.ts
+++ src/compose.ts
@@ -7,20 +7,20 @@ export function compose<A, B, C>(fn1: Fn1<A, B>, fn2: Fn1<B, C>): Fn1<A, C>;
 export function compose<A, B, C, D>(
   fn1: Fn1<A, B>,
   fn2: Fn1<B, C>,
-  fn3: Fn1<C, D>
+  fn3: Fn1<C, D>,
 ): Fn1<A, D>;
 export function compose<A, B, C, D, E>(
   fn1: Fn1<A, B>,
   fn2: Fn1<B, C>,
   fn3: Fn1<C, D>,
-  fn4: Fn1<D, E>
+  fn4: Fn1<D, E>,
 ): Fn1<A, E>;
 export function compose<A, B, C, D, E, F>(
   fn1: Fn1<A, B>,
   fn2: Fn1<B, C>,
   fn3: Fn1<C, D>,
   fn4: Fn1<D, E>,
-  fn5: Fn1<E, F>
+  fn5: Fn1<E, F>,
 ): Fn1<A, F>;
 export function compose<A, B, C, D, E, F, G>(
   fn1: Fn1<A, B>,
@@ -28,7 +28,7 @@ export function compose<A, B, C, D, E, F, G>(
   fn3: Fn1<C, D>,
   fn4: Fn1<D, E>,
   fn5: Fn1<E, F>,
-  fn6: Fn1<F, G>
+  fn6: Fn1<F, G>,
 ): Fn1<A, G>;
 export function compose<A, B, C, D, E, F, G, H>(
   fn1: Fn1<A, B>,
@@ -37,7 +37,7 @@ export function compose<A, B, C, D, E, F, G, H>(
   fn4: Fn1<D, E>,
   fn5: Fn1<E, F>,
   fn6: Fn1<F, G>,
-  fn7: Fn1<G, H>
+  fn7: Fn1<G, H>,
 ): Fn1<A, H>;
 export function compose<A, B, C, D, E, F, G, H, I>(
   fn1: Fn1<A, B>,
@@ -47,7 +47,7 @@ export function compose<A, B, C, D, E, F, G, H, I>(
   fn5: Fn1<E, F>,
   fn6: Fn1<F, G>,
   fn7: Fn1<G, H>,
-  fn8: Fn1<H, I>
+  fn8: Fn1<H, I>,
 ): Fn1<A, I>;
 export function compose<TInput, TOutput>(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
